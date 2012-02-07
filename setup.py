b6901b32f04064c453514afda12c89c63946d865
Camille Boissel <camille@semio-madmoizelle.local>
2012-02-07 18:37:45 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from os import path
+from setuptools import setup, find_packages
+
+with open(path.join(path.dirname(__file__), 'README')) as f:
+    readme = f.read()
+
+setup(
+    name="django-bbcode",
+    version='1.0.0',
+    description="Little helper application to parse and render bbcode",
+    long_description=readme,
+    url="https://github.com/camilleb/django-bbcode",
+    author="marcinn (Marcin Nowak)",
+    packages=find_packages(),
+    include_package_data=True,
+)
